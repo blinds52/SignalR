6c577274df85b7ecb4bed94b2665f735fd935feb
Pawel Kadluczka <pawelka@microsoft.com>
2017-05-18 09:50:36 -0700

    Fixing WebSocketsTransport to handle exceptions correctly
    Exceptions thrown when sending or receiving messages would leave the
    WebSockets transport in a half-closed state when one of the loops is
    closed but the other one is still running preventing from the
    Connection.Closed event to be fired.
    
    Fixes: #412

--- src/Microsoft.AspNetCore.Sockets.Client/LongPollingTransport.cs
+++ src/Microsoft.AspNetCore.Sockets.Client/LongPollingTransport.cs
@@ -6,7 +6,6 @@ using System.Buffers;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Sockets.Internal.Formatters;
